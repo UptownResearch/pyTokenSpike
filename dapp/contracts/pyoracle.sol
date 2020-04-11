e9fd9a4507f3325593fffd15fa478ebdbabbd3eb
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-04-11 15:10:46 +0100

    Fixed most linting in pyOracle.sol

--- dapp/contracts/pyoracle.sol
+++ dapp/contracts/pyoracle.sol
@@ -1,19 +1,15 @@
 pragma solidity ^0.5.1;
 
 
-
 contract pyOracle {
 
     address public underlying;
     address public collateral;
-    constructor ( 
-            address _underlying,
-            address _collateral
+    constructor (
+        address _underlying,
+        address _collateral
     ) public {
         underlying = _underlying;
         collateral = _collateral;
     }
-
-
-
 }
